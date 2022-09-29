59366e13bd205a0ac7b5b63a1752b6cc7f6862c2
Nishant Mittal <nishantmittal2002@gmail.com>
2022-09-29 17:13:04 +0530

    edit profile implemented

--- app/src/main/java/com/bpitindia/attendance/ProfileFragment.kt
+++ app/src/main/java/com/bpitindia/attendance/ProfileFragment.kt
@@ -8,25 +8,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
-import androidx.navigation.fragment.findNavController
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import okhttp3.*
-import org.json.JSONArray
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody.Companion.toRequestBody
 import org.json.JSONObject
 import java.io.IOException
 
 private const val PROFILE = "profile"
+private const val SHARED_PREFERENCES_NAME = "shared_pref"
+private const val SHARED_PREFERENCES_TOKEN_KEY = "token"
 
 class ProfileFragment : Fragment() {
+    private var sharedPreferences: SharedPreferences? = null
     private var profile: String? = null
+    private lateinit var jsonObject: JSONObject
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             profile = it.getString(PROFILE)
         }
+        jsonObject = JSONObject(profile!!)
+        sharedPreferences =
+            activity?.getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE)
     }
 
     override fun onCreateView(
@@ -43,19 +51,96 @@ class ProfileFragment : Fragment() {
         view.findViewById<Button>(R.id.logout_button).setOnClickListener {
             (activity as MainActivity).logout()
         }
+        view.findViewById<ImageButton>(R.id.edit_name_button).setOnClickListener {
+            showAlertDialog("Name", view)
+        }
+        view.findViewById<ImageButton>(R.id.edit_phone_button).setOnClickListener {
+            showAlertDialog("Phone Number", view)
+        }
         setProfile(view)
     }
 
     private fun setProfile(view: View) {
-        val jsonObject = profile?.let { JSONObject(it) }
-        if (jsonObject != null) {
-            view.findViewById<TextView>(R.id.name_profile).text = jsonObject.getString("name")
-            view.findViewById<TextView>(R.id.email_profile).text = jsonObject.getString("email")
-            view.findViewById<TextView>(R.id.phone_profile).text = jsonObject.getString("phone_number")
-            view.findViewById<TextView>(R.id.designation_profile).text = jsonObject.getString("designation")
-            view.findViewById<TextView>(R.id.doj_profile).text = jsonObject.getString("date_joined")
+
+        view.findViewById<TextView>(R.id.name_profile).text = jsonObject.getString("name")
+        view.findViewById<TextView>(R.id.email_profile).text = jsonObject.getString("email")
+        view.findViewById<TextView>(R.id.phone_profile).text = jsonObject.getString("phone_number")
+        view.findViewById<TextView>(R.id.designation_profile).text =
+            jsonObject.getString("designation")
+        view.findViewById<TextView>(R.id.doj_profile).text = jsonObject.getString("date_joined")
+    }
+
+    private fun showAlertDialog(field: String, view: View) {
+        val edittext = EditText(context)
+        var textview: TextView? = null
+        when (field) {
+            "Name" -> textview = view.findViewById(R.id.name_profile)
+            "Phone Number" -> textview = view.findViewById(R.id.phone_profile)
+        }
+        edittext.setText(textview?.text)
+        edittext.maxLines = 1
+
+        val layout = FrameLayout(requireContext())
+        layout.setPaddingRelative(45, 25, 45, 0)
+        layout.addView(edittext)
+
+        AlertDialog.Builder(requireContext()).apply {
+            setTitle("Edit $field")
+            setView(layout)
+            setPositiveButton("Save") { _, _ ->
+                val newValue = edittext.text.toString()
+                textview?.text = newValue
+                when (field) {
+                    "Name" -> jsonObject.put("name", newValue)
+                    "Phone Number" -> jsonObject.put("phone_number", newValue)
+                }
+                val token = sharedPreferences?.getString(SHARED_PREFERENCES_TOKEN_KEY, null)
+                updateProfile(token!!)
+            }
+            setNegativeButton("Discard") { _, _ -> }
+
+        }.create().show()
+    }
+
+    private fun updateProfile(token: String) {
+        val url = getString(R.string.profile_api_url)
+        val client = OkHttpClient()
+        lifecycleScope.launch(Dispatchers.IO) {
+            val mediaType = "application/json; charset=utf-8".toMediaType()
+            val body = jsonObject.toString().toRequestBody(mediaType)
+            val request = Request.Builder().url(url).patch(body).addHeader("Authorization", token).build()
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    activity?.runOnUiThread {
+                        Toast.makeText(
+                            context,
+                            "Profile Update API Failed",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                    Log.d("debug", "profile update failed")
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    Log.d("debug", "profile update response ${response.message}")
+                    if (response.isSuccessful) {
+                        Log.d("debug", "profile loading successful")
+                        (activity as MainActivity).fetchProfile(token)
+                    } else {
+                        Log.d("debug", "profile update failed ${response.message}")
+                        activity?.runOnUiThread {
+                            Toast.makeText(
+                                context,
+                                "Profile Update Failed",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                }
+            })
         }
     }
+
     override fun onDestroyView() {
         super.onDestroyView()
         (activity as MainActivity).setDrawerUnlocked()
